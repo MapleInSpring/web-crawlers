1abb96bdfca66716a75f97feb0684db180cf9253
Pivotal <pivotal@iMac-3.local>
2019-04-10 16:22:56 +0800

    Use splash to render js based page

--- README.md
+++ README.md
@@ -11,3 +11,8 @@ Run splash locally
 ### Managing local python environment and packages with Conda
 [Conda cheatsheet](https://docs.conda.io/projects/conda/en/4.6.0/_downloads/52a95608c49671267e40c689e0bc00ca/conda-cheatsheet.pdf)
 
+
+### Other ways to render JS pages
+[Selenium python](https://stanford.edu/~mgorkove/cgi-bin/rpython_tutorials/Scraping_a_Webpage_Rendered_by_Javascript_Using_Python.php)
+
+[PhantomJS](http://phantomjs.org/api/webpage/method/render.html)
--- myspiders/spiders/kubepartners.py
+++ myspiders/spiders/kubepartners.py
@@ -2,21 +2,33 @@
 import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
+from scrapy_splash import SplashRequest
 
 
 class KubepartnersSpider(CrawlSpider):
     name = 'kubepartners'
-    allowed_domains = ['https://kubernetes.io/partners/#kcsp']
-    start_urls = ['http://https://kubernetes.io/partners/#kcsp/']
 
-    rules = (
-        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
-    )
+    def start_requests(self):
+        urls = [
+            'https://kubernetes.io/partners/#kcsp',
+            'https://landscape.cncf.io/category=kubernetes-certified-service-provider&format=card-mode&grouping=category&embed=yes'
+            ]
+        for url in urls:
+            yield SplashRequest(url=url, callback=self.parse)
 
-    def parse_item(self, response):
+    def parse(self, response):
         i = {}
         #i['domain_id'] = response.xpath('//input[@id="sid"]/@value').extract()
         #i['name'] = response.xpath('//div[@id="name"]').extract()
         #i['description'] = response.xpath('//div[@id="description"]').extract()
         # get all the partners
+        divs = response.xpath('//div').getall()
+        div_number = len(divs)
+        self.logger.info('total number of divs {0}'.format(div_number))
+
+        # debug with shell
+        # from scrapy.shell import inspect_response
+        # inspect_response(response, self)
+
+        self.logger.info('hello world')
         return i
